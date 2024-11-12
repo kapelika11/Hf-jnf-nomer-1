0688919291ed41cd29f5c124c1539353afde74e8
Cybersport <Cybersport@26kadr.internal>
2024-11-12 11:31:57 +0300

    js

---
+++ main.js
@@ -0,0 +1,52 @@
+// let admin задание 2
+// let name = 'Даня'
+// admin = name
+// alert(admin);
+
+// let name = "Ilya"   задание 3
+// alert( hello ${1} ); // ?
+// alert( hello ${"name"} ); // ?
+// alert( hello ${name} ); // ?
+
+// задание 4
+// var name = prompt("Как вас зовут?") ;
+// document.write("Привет, " + Name + "!") ;
+
+// задание 5
+// let a = 1, b = 1;
+// let c = ++a; // ?
+// let d = b++; //?
+
+// задание 6
+// let a = 2;
+// let x = 1 + (a *= 2);
+// alert(a+b);
+
+// задание 7
+// let a = parseInt(prompt("Первое число?", 1));
+// let b = parseInt(prompt("Второе число?", 2));
+// alert(a+b); //12
+
+
+// задание 8
+// 7 > 5
+// "стол" > стул
+// "4"  >"52"
+// undefined ==null
+// undefined === null
+// null == "\n0\n"
+// null ===+ "\n0\n"
+
+// Задание 9
+// if ("0") {
+//     alert('Привет');
+// }
+
+
+// задание 10
+// let name = prompt("Какое «официальное» название JavaScript?");
+// if (name === "ECMAScript") {
+//     alert("Верно!");
+// } else {
+//     alert("Не знаете? ECMAScript!");
+// }
